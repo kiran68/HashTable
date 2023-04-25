62991a693d2f317dd146f684b821fcdb24ccbaa3
kiran68 <kiranurshil88@gmail.com>
2023-04-25 23:12:09 +0530

    Add:Check Word Is Remove Or Not

--- src/com/bridgelabz/hashtable/HashTable.java
+++ src/com/bridgelabz/hashtable/HashTable.java
@@ -3,31 +3,35 @@ package com.bridgelabz.hashtable;
 import java.util.Hashtable;
 
 public class HashTable {
-	public static void main (String [] args) {
-        HashMapTableImp<String,Integer> hashImpl = new HashMapTableImp<>();
-		String massage = "Paranoids are not paranod because they are paranoid but" + 
-		                 "because they keep putting themeselves deliberately init paranoid available situtation";
-		
-		//Split the words when a white space comes up.
-		//Convert all the words in lowercase
-		String[] messageArray = massage.toLowerCase().split(" ");
-		
-		for (String word:messageArray) {
-			//Search for the word in the linked list
-			
-			Integer value = hashImpl.get(word);
-			//If not Found,Frequency of the word StayWith 1 
-			
-			if (value == null)
-			value = 1;
-			
-			else 
-				//If found, frequency of the word increases by 1
-				value = value + 1;
-			hashImpl.add(word,value);
-			
-		}
-		System.out.println(hashImpl);
-	}
+
+    public static void main(String[] args) {
+        HashMapTableImp<String, Integer> hashImpl = new HashMapTableImp<>();
+        String message = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+
+        //Split the words when a white space comes up.
+        //Convert all the words in lowercase
+        String[] messageArray = message.toLowerCase().split(" ");
+
+        for(String word: messageArray) {
+            //Search for the word in the hashtable
+            Integer value =  hashImpl.get(word);
+            //If not found, frequency of the word StayWith 1
+            if( value == null)
+                value = 1;
+            else
+                //If found, frequency of the word increases by 1
+                value = value + 1;
+            hashImpl.add(word , value);
+            System.out.println("Value = "+value);
+        }
+        System.out.println(hashImpl);
+
+        //Remove "avoidable" from the hashtable
+        hashImpl.remove("avoidable");
+
+        //Display the hashtable
+        System.out.println(hashImpl);
+    }
 }
 
